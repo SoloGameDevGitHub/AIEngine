4fd14f3ce4195e0504091b509ef89c74b4e09404
Ândrei Schuch <sologamedevmail@gmail.com>
2019-05-19 20:56:44 -0300

    Pushing some more evaluate tests

--- AdversarialSearch/Minimax/Main.cpp
+++ AdversarialSearch/Minimax/Main.cpp
@@ -9,6 +9,8 @@ int main()
 	benchmarker -> benchmarkMinimaxVsMinimax(minimax, 0, true);
 	benchmarker -> benchmarkEvaluate(minimax, 0, true);
 	benchmarker -> benchmarkEvaluate(minimax, 0b100000110000100011, true);
+	benchmarker -> benchmarkEvaluate(minimax, 0b100000110011100011, false);
+	benchmarker -> benchmarkEvaluate(minimax, 0b100000110011101011, true);
 	
 	return 0;
 };
