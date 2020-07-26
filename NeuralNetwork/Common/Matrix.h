b316bd2691d37844dfb129b476733d5e184d4e1e
andreimesquita <andreirs@outlook.com>
2020-07-26 19:29:27 -0300

    Updating Perceptron and Matrix code to use smart pointers

--- NeuralNetwork/Common/Matrix.h
+++ NeuralNetwork/Common/Matrix.h
@@ -2,33 +2,35 @@
 #define NEURALNETWORK_MATRIX_H
 #include <ctime>
 #include <iostream>
+#include <math.h>
 #include <cmath>
 #include <cassert>
+#include <vector>
+#include <memory>
 
 typedef float (*FloatFunction)(float);
 
 class Matrix
 {
 private:
-    float* _values;
+    std::vector<float> _values;
     int _rows;
     int _columns;
 
 public:
-    Matrix(const int rows, const int columns);
-    ~Matrix();
-    float get(const int row, const int column) const;
-    void set(const int row, const int column, const float value);
+    Matrix(int rows, int columns);
+    Matrix(const Matrix &source);
+    float get(int row, int column) const;
+    void set(int row, int column, float value);
     int getRows() const;
     int getColumns() const;
     void print(std::ostream& stream);
-    void print(std::ostream& stream, int decimalPlace);
-    Matrix* clone();
+    void print(std::ostream& stream, int decimalPlace) const;
+    static void applyFunction(FloatFunction function, const Matrix& source, Matrix&  target);
     void applyFunction(FloatFunction function);
-
-    static Matrix* multiply(const Matrix& left, const Matrix& right);
+    static std::unique_ptr<Matrix> multiply(const Matrix& left, const Matrix& right);
     static void multiply(const Matrix& left, const Matrix& right, Matrix& target);
-    static void multiply(Matrix& target, const float scalar);
-    static void add(Matrix& target, const float value);
+    static void multiply(Matrix& target, float scalar);
+    static void add(Matrix& target, float value);
 };
 #endif //NEURALNETWORK_MATRIX_H
