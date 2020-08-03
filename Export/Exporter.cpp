da1113ff50127a71ee166995335911ec65f24fcd
andreimesquita <andreirs@outlook.com>
2020-08-03 16:41:31 -0300

    Adding library and export option

---
+++ Export/Exporter.cpp
@@ -0,0 +1,20 @@
+#ifndef AIENGINE_EXPORTER_C
+#define AIENGINE_EXPORTER_C
+
+#ifdef _EXPORT_DLL_
+#define API __declspec(dllexport)
+#else
+#define API __declspec(dllimport)
+#endif // _EXPORT_DLL_
+
+#include "../AdversarialSearch/Minimax/BitMath.h"
+#include "../AdversarialSearch/Minimax/TicTacToeMinimax.h"
+#include "../NeuralNetwork/Common/Random.h"
+#include "../NeuralNetwork/Common/Matrix.h"
+#include "../NeuralNetwork/Common/ActivationFunctions.h"
+#include "../NeuralNetwork/Perceptron/Neuron.h"
+#include "../NeuralNetwork/Perceptron/Perceptron.h"
+#include "../NeuralNetwork/MLP/Layer.h"
+#include "../NeuralNetwork/MLP/MLP.h"
+
+#endif //AIENGINE_EXPORTER_C
