3a01849d5825ffbf1d14e892e8ed3cdee1db7329
andreimesquita <andreirs@outlook.com>
2020-11-28 22:26:03 -0300

    Implementing simple methods in the export API (working example)

--- Export/Exporter.cpp
+++ Export/Exporter.cpp
@@ -2,14 +2,17 @@
 #define API __declspec(dllexport)
 #else
 #define API __declspec(dllimport)
-#endif // _EXPORT_DLL_
+#endif
 
-#include "../AdversarialSearch/Minimax/BitMath.h"
-#include "../AdversarialSearch/Minimax/TicTacToeMinimax.h"
-#include "../NeuralNetwork/Common/Random.h"
-#include "../NeuralNetwork/Common/Matrix.h"
-#include "../NeuralNetwork/Common/ActivationFunctions.h"
-#include "../NeuralNetwork/Perceptron/Neuron.h"
-#include "../NeuralNetwork/Perceptron/Perceptron.h"
-#include "../NeuralNetwork/MLP/Layer.h"
-#include "../NeuralNetwork/MLP/MLP.h"
+extern "C"
+{
+API int sum(int lhs, int rhs)
+{
+    return lhs + rhs;
+}
+
+API int getOne()
+{
+    return 1;
+}
+}
