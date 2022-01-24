dea42149e267e54bb02ebc7d6e76029c62521632
AndreiSchuch <andreirs@outlook.com>
2022-01-24 15:41:53 -0300

    Creating a MathLibrary to be used as an example when exporting shared libraries from this project

---
+++ Export/MathLibrary.cpp
@@ -0,0 +1,16 @@
+#include "MathLibrary.h"
+
+int sum(int lhs, int rhs)
+{
+    return lhs + rhs;
+}
+
+int getOne()
+{
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ Export/MathLibrary.h
@@ -0,0 +1,12 @@
+// MathLibrary.h - Contains declarations of math functions
+#pragma once
+
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+extern "C" MATHLIBRARY_API int sum(int lhs, int rhs);
+
+extern "C" MATHLIBRARY_API int getOne();
